d40243c73f0a692c561722b41e7fa1b0ebab96d4
Eden <edenzha@umich.edu>
2025-09-02 14:28:46 -0400

    update .sh file

--- run_my_job.sh
+++ run_my_job.sh
@@ -3,9 +3,12 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --partition=gpu
 #SBATCH --time=02:00:00
-#SBATCH --gres=gpu:A6000:2 
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --mem=32G
 #SBATCH --chdir=/home/edenzha/flow_of_argument
 
 
-
-python extract_assumption.py 
+python extract_assumption.py \
+  --data_path /home/edenzha/flow_of_argument/data/covid_episodes.jsonl.gz \
+  --sample_n 30 \
+  --min_words 50 \
