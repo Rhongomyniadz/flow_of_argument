2b47edac175f7c6d8212edccce589c5217caeb09
Rhongomyniadz <edenzha@umich.edu>
2025-09-08 20:32:53 -0400

    modify logic

--- job.sh
+++ job.sh
@@ -7,10 +7,6 @@
 #SBATCH --mem=32G
 #SBATCH --chdir=/home/edenzha/flow_of_argument
 
-export VLLM_ATTENTION_BACKEND=TORCH_SDPA
-export VLLM_USE_FLASH_ATTN=0
-export VLLM_USE_FUSED_RMSNORM=0
-export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
 python extract_assumption.py \
   --data_path  /home/edenzha/flow_of_argument/data/covid_episodes.jsonl.gz \
