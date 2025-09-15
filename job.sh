9e480190b85420d82d996ec56b310e49b6ea342b
Eden <edenzha@umich.edu>
2025-09-15 16:03:22 -0400

    update job.sh

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=slurm-%j.out
 #SBATCH --partition=gpu
 #SBATCH --time=36:00:00
-#SBATCH --gres=gpu:A6000:4
+#SBATCH --gres=gpu:A6000:2
 #SBATCH --mem=64G
 #SBATCH --chdir=/home/edenzha/flow_of_argument
 
