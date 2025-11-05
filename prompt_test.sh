22d42709468c5b310cacaef7b34e2c60d1344558
Rhongomyniadz <edenzha@umich.edu>
2025-11-05 00:41:15 -0500

    add .sh file

---
+++ prompt_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=assumption_extraction
+#SBATCH --output=slurm-%j.out
+#SBATCH --partition=gpu
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:A6000:2
+#SBATCH --mem=64G
+#SBATCH --chdir=/home/edenzha/flow_of_argument
+
+
+python prompt_test.py
