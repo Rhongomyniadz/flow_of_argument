a77e92d23c4df70e8af6c66336640a1e62b8c191
Rhongomyniadz <edenzha@umich.edu>
2025-12-11 23:21:51 +0800

    upload new model

--- pipeline.sh
+++ pipeline.sh
@@ -8,4 +8,4 @@
 #SBATCH --mem=64G
 #SBATCH --chdir=/home/edenzha/flow_of_argument
 
-python pipeline.py --tensor_parallel_size 2 --batch_size 32 --num_episodes 500
+python pipeline.py --tensor_parallel_size 2 --batch_size 32 --num_episodes 5000
