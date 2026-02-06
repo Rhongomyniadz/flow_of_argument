03c1e121ba2dcb8fa10ee7dabf265ff3a3a4a454
Rhongomyniadz <edenzha@umich.edu>
2026-02-06 18:29:47 -0500

    fix

--- turn_type_labeler.sh
+++ turn_type_labeler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=llm_as_a_judge
-#SBATCH --output=_log/llm_as_a_judge.out
+#SBATCH --job-name=stance_labeler
+#SBATCH --output=_log/turn_type_labeler.out
 #SBATCH --partition=gpu
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
