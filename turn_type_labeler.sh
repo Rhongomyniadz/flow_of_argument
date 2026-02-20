4370882a775e15366b144ab7ce98327ac3aafa16
Rhongomyniadz <edenzha@umich.edu>
2026-02-20 13:09:56 -0500

    request A6000

--- turn_type_labeler.sh
+++ turn_type_labeler.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=stance_labeler
+#SBATCH --job-name=turn_type_labeler
 #SBATCH --output=_log/turn_type_labeler.out
 #SBATCH --partition=gpu
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:A100:2
+#SBATCH --gres=gpu:A6000:2
 #SBATCH --mem=64G
 #SBATCH --chdir=/home/edenzha/flow_of_argument
 
